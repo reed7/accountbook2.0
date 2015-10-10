9b2568d6c444ce163b3b1583a3448de6415a6e5b
reed7 <reeed7@gmail.com>
2015-10-09 20:55:46 -0400

    css path

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,27 +5,27 @@
 
 <?php switch($this->innerPage){
 	case Controller::PAGE_LOGIN:?>
-	<link rel="stylesheet" href="/accountbook/css/login.css" type="text/css"></link>
+	<link rel="stylesheet" href="<?php echo Yii::app()->getBaseUrl();?>/css/login.css" type="text/css"></link>
 	<?php 
 	break;
 	case Controller::PAGE_STATISTIC :
 	?>
 	<link rel="stylesheet" href='<?php echo Yii::app()->getBaseUrl();?>/css/statistic.css' type="text/css"></link>
-	<link rel="stylesheet" href="/accountbook/css/account_book.css" type="text/css"></link>
-	<link rel="stylesheet" href="/accountbook/css/account_item_detail.css" type="text/css"></link>
+	<link rel="stylesheet" href="<?php echo Yii::app()->getBaseUrl();?>/css/account_book.css" type="text/css"></link>
+	<link rel="stylesheet" href="<?php echo Yii::app()->getBaseUrl();?>/css/account_item_detail.css" type="text/css"></link>
 	<?php 
 	break;
 	default : ?>
-	<link rel="stylesheet" href="/accountbook/css/account_book.css" type="text/css"></link>	
-	<link rel="stylesheet" href="/accountbook/css/account_item_detail.css" type="text/css"></link>
-	<link rel="stylesheet" href="/accountbook/css/statistic_time_range.css" type="text/css"></link>	
-	<link rel="stylesheet" href="/accountbook/assets/b53eeb24/jui/css/base/jquery-ui.css" type="text/css" ></link>
+	<link rel="stylesheet" href="<?php echo Yii::app()->getBaseUrl();?>/css/account_book.css" type="text/css"></link>	
+	<link rel="stylesheet" href="<?php echo Yii::app()->getBaseUrl();?>/css/account_item_detail.css" type="text/css"></link>
+	<link rel="stylesheet" href="<?php echo Yii::app()->getBaseUrl();?>/css/statistic_time_range.css" type="text/css"></link>	
+	<link rel="stylesheet" href="<?php echo Yii::app()->getBaseUrl();?>/assets/b53eeb24/jui/css/base/jquery-ui.css" type="text/css" ></link>
 	
-	<script type="text/javascript" src="/accountbook/assets/b53eeb24/jquery.js"></script>
-	<script type="text/javascript" src="/accountbook/assets/b53eeb24/jui/js/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="<?php echo Yii::app()->getBaseUrl();?>/assets/b53eeb24/jquery.js"></script>
+	<script type="text/javascript" src="<?php echo Yii::app()->getBaseUrl();?>/assets/b53eeb24/jui/js/jquery-ui.min.js"></script>
 <?php }// end switch?>
 
-<script type="text/javascript" src="/accountbook/js/main.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->getBaseUrl();?>/js/main.js"></script>
 </head>
 <body>
 <?php echo $content;?>
